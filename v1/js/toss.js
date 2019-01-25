e26c9d50cca0eb54ec302ca265d02b7da12613d1
Tejasvi <karne.teja@gmail.com>
2019-01-25 12:10:28 +0530

    Update toss.js

--- v1/js/toss.js
+++ v1/js/toss.js
@@ -4,4 +4,46 @@
   function setUp(){
     
   }
+
+
+
+window.addEventListener('load', function(){
+  var body = document.body,
+      nav = document.getElementsByTagName('nav')[0],
+      toggleMenu = nav.querySelector('.toggle-menu'),
+      menu = nav.querySelector('#primary-menu');
+
+  function showMenu(){
+    var initialHeight = nav.getBoundingClientRect().height;
+    toggleMenu.setAttribute('aria-expanded', 'true');
+    body.classList.add('showing-menu');
+
+/* menu.style.display = "block";
+nav.getBoundingClientRect().height;
+menu.style.display = "none"; */
+
+  }
+
+  function hideMenu(){
+    var initialHeight = nav.getBoundingClientRect().height;
+    toggleMenu.setAttribute('aria-expanded', 'false');
+    body.classList.remove('showing-menu');
+  }
+
+  toggleMenu.addEventListener('click', function(){
+    var expanded = this.getAttribute('aria-expanded'),
+        body = document.body;
+
+    if(expanded === 'false'){
+      showMenu();
+    }
+    else{
+      hideMenu();
+    }
+  });
+});
+
+
+
+
 })()
